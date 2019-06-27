488d795e1ae4e826b587eec50a6ea3a5413e84d9
Carmelo Uria <curia@carmelouria.com>
2019-06-27 11:19:05 -0600

    Initial Commit

---
+++ MicroServiceManager/MicroServiceManager.h
@@ -0,0 +1,19 @@
+//
+//  MicroServiceManager.h
+//  MicroServiceManager
+//
+//  Created by Carmelo Uria on 6/27/19.
+//  Copyright © 2019 Carmelo Uria Corporation. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for MicroServiceManager.
+FOUNDATION_EXPORT double MicroServiceManagerVersionNumber;
+
+//! Project version string for MicroServiceManager.
+FOUNDATION_EXPORT const unsigned char MicroServiceManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MicroServiceManager/PublicHeader.h>
+
+
